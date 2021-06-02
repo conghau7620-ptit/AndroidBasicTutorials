f6df6976e855082610b751a60f1ae9b4ffedc66b
conghau7620-ptit <conghau7620.ptit@gmail.com>
2021-06-02 22:08:12 +0700

    update EditText Tutorials

---
+++ Static UI/View/EditText/EditTextSample/app/src/main/java/ptit/hau/edittextsample/MainActivity.kt
@@ -0,0 +1,28 @@
+package ptit.hau.edittextsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                textView.setText("Text in EditText: " + s)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+    }
+}
---
+++ Static UI/View/EditText/README.md
@@ -0,0 +1,3 @@
+# EditText
+EditText Tutorials & Samples <br />
+View Tutorial here: https://www.notion.so/EditText-a6056321f5d54e41947b28b88baa086c
