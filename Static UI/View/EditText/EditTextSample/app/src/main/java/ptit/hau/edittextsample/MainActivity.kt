7b93b199e186656cd9d764878e60ed2048a48d4b
conghau7620-ptit <conghau7620.ptit@gmail.com>
2021-06-04 09:49:48 +0700

    update EditText Tutorials

--- Static UI/View/EditText/EditTextSample/app/src/main/java/ptit/hau/edittextsample/MainActivity.kt
+++ Static UI/View/EditText/EditTextSample/app/src/main/java/ptit/hau/edittextsample/MainActivity.kt
@@ -1,10 +1,14 @@
 package ptit.hau.edittextsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText.addTextChangedListener(object : TextWatcher{
+        editText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 textView.setText("Text in EditText: " + s)
             }
@@ -24,5 +28,41 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+
+        edtUser.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString().length == 0) {
+                    inputLayoutUser.setError("User không được bỏ trống")
+                }
+                else {
+                    inputLayoutUser.setError(null)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+
+        edtPass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString().length == 0) {
+                    inputLayoutPass.setError("Password không được bỏ trống")
+                } else {
+                    inputLayoutPass.setError(null)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+
     }
 }
--- Static UI/View/TextView/TextViewSample/app/src/main/java/ptit/hau/textview/MainActivity.kt
+++ Static UI/View/TextView/TextViewSample/app/src/main/java/ptit/hau/textview/MainActivity.kt
@@ -1,12 +1,25 @@
 package ptit.hau.textview
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.setText(R.string.hello)
+
+        var leftDrawable = getDrawable(R.drawable.ic_snow)
+        var topDrawable = getDrawable(R.drawable.ic_snow)
+        var righthtDrawable = getDrawable(R.drawable.ic_snow)
+        var bottomDrawable = getDrawable(R.drawable.ic_snow)
+
+        specialTextView.setCompoundDrawablesWithIntrinsicBounds(leftDrawable,topDrawable,
+                                            righthtDrawable,bottomDrawable)
     }
 }
