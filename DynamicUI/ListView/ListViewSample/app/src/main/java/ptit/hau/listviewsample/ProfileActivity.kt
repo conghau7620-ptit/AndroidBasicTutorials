233a6c8aa4beb2d1fa79f34fc34da0c05deff123
conghau7620-ptit <conghau7620.ptit@gmail.com>
2021-06-25 16:19:52 +0700

    update ListViewSample

--- DynamicUI/ListView/ListViewSample/app/src/main/java/ptit/hau/listviewsample/ProfileActivity.kt
+++ DynamicUI/ListView/ListViewSample/app/src/main/java/ptit/hau/listviewsample/ProfileActivity.kt
@@ -1,16 +1,21 @@
 package ptit.hau.listviewsample
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_profile.*
 import java.io.Serializable
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ProfileActivity : AppCompatActivity(){
 
     var flag = false
+    var date = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,24 +27,34 @@ class ProfileActivity : AppCompatActivity(){
         imgSinger2.setImageResource(singer.avatar)
         edtName2.setText(singer.name)
         edtBand2.setText(singer.band)
-        edtBirthday2.setText(singer.birthDay.toString())
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+        edtBirthday2.setText(dateFormat.format(date.time))
+        edtBirthday2.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                pickDate()
+            }
+        })
 
         btnEdit.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 if (!flag) {
                     edtName2.isEnabled = true
                     edtBand2.isEnabled = true
+                    edtBirthday2.isEnabled = true
                     btnEdit.setText("Save")
                     flag = true
                 }
                 else {
                     edtName2.isEnabled = false
                     edtBand2.isEnabled = false
+                    edtBirthday2.isEnabled = false
                     btnEdit.setText("Edit")
                     flag = false
 
                     singer.name = edtName2.text.toString()
                     singer.band = edtBand2.text.toString()
+                    singer.birthDay = date
+
                     MainActivity.listSinger[pos] = singer
                     MainActivity.listSingerAdapter!!.notifyDataSetChanged()
 
@@ -47,5 +62,15 @@ class ProfileActivity : AppCompatActivity(){
             }
         })
 
+    }
+
+    fun pickDate() {
+        val datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
+                view, year, month, day ->
+                    val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+                    date.set(year,month,day)
+                    edtBirthday2.setText(dateFormat.format(date.time))
+        },date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE))
+        datePickerDialog.show()
     }
 }
--- DynamicUI/ListView/ListViewSample/app/src/main/java/ptit/hau/listviewsample/Singer.kt
+++ DynamicUI/ListView/ListViewSample/app/src/main/java/ptit/hau/listviewsample/Singer.kt
@@ -3,7 +3,7 @@ package ptit.hau.listviewsample
 import java.io.Serializable
 import java.util.*
 
-class Singer (val id: Int, var name: String, var band: String, val birthDay : Date?,
+class Singer (val id: Int, var name: String, var band: String, var birthDay : Calendar?,
               val avatar: Int){
 
 }
